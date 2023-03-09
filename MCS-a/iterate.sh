be96150cb61acbd4197d0457a07fdb8b842b1b9c
guoyuanjing <73580243+guoyuanjing@users.noreply.github.com>
2023-03-09 21:24:56 +0800

    Add files via upload

---
+++ MCS-a/iterate.sh
@@ -0,0 +1,178 @@
+#!/bin/bash
+#SBATCH --job-name=0529          # Specify job name
+#SBATCH --partition=shared     # Specify partition name
+#SBATCH --ntasks=1             # Specify max. number of tasks to be invoked
+#SBATCH --cpus-per-task=1      # Specify number of CPUs per task
+#SBATCH --time=08:00:00        # Set a limit on the total run time
+#SBATCH --account=bm0982       # Charge resources on this project account
+#SBATCH --output=track0529.o%j    # File name for standard output
+#SBATCH --error=track0529.o%j     # File name for standard error output
+#shopt -s expand_aliases
+#source ~/.bashrc
+
+set -ex
+
+   PFAD=`pwd`
+
+
+
+for year in $(seq 2020 2020)
+# for year in $(seq 2001 2020)
+do 
+
+FILENAMESTRING='gpmmm_cn_'${year}'07'
+cd $PFAD
+${PFAD}/irt_objects_release.x ${FILENAMESTRING}
+${PFAD}/irt_tracks_release.x ${FILENAMESTRING}
+${PFAD}/irt_tracklinks_release.x ${FILENAMESTRING}
+cd /disk1/guoyj/MCS-tracking/iterative_raincell_tracking/GPM-225K-6hr-a-a/output/
+sort -n -k2 irt_tracks_nohead_output_${FILENAMESTRING}.txt > irt_tracks_sorted_${FILENAMESTRING}.txt
+${PFAD}/irt_trackmask_release.x ${FILENAMESTRING}
+#cdo -r -f nc copy irt_objects_mask_${FILENAMESTRING}.srv irt_objects_mask_${FILENAMESTRING}.nc
+cdo -r -f nc copy irt_tracks_mask_ccs_${FILENAMESTRING}.srv irt_tracks_mask_ccs_${FILENAMESTRING}.nc
+cdo -r -f nc copy irt_tracks_mask_${FILENAMESTRING}.srv irt_tracks_mask_${FILENAMESTRING}.nc
+rm irt_objects_mask_${FILENAMESTRING}.srv
+rm irt_tracks_mask_ccs_${FILENAMESTRING}.srv
+rm irt_tracks_mask_${FILENAMESTRING}.srv
+
+
+
+
+
+
+
+# for year in $(seq 2008 2008)
+# # for year in $(seq 2001 2020)
+# do 
+
+# FILENAMESTRING='gpmmm_cn_'${year}'06'
+# cd $PFAD
+# ${PFAD}/irt_objects_release.x ${FILENAMESTRING}
+# ${PFAD}/irt_tracks_release.x ${FILENAMESTRING}
+# ${PFAD}/irt_tracklinks_release.x ${FILENAMESTRING}
+# cd /disk1/guoyj/MCS-tracking/iterative_raincell_tracking/GPM-225K-6hr-a-a/output/
+# sort -n -k2 irt_tracks_nohead_output_${FILENAMESTRING}.txt > irt_tracks_sorted_${FILENAMESTRING}.txt
+# ${PFAD}/irt_trackmask_release.x ${FILENAMESTRING}
+# #cdo -r -f nc copy irt_objects_mask_${FILENAMESTRING}.srv irt_objects_mask_${FILENAMESTRING}.nc
+# cdo -r -f nc copy irt_tracks_mask_ccs_${FILENAMESTRING}.srv irt_tracks_mask_ccs_${FILENAMESTRING}.nc
+# cdo -r -f nc copy irt_tracks_mask_${FILENAMESTRING}.srv irt_tracks_mask_${FILENAMESTRING}.nc
+# rm irt_objects_mask_${FILENAMESTRING}.srv
+# rm irt_tracks_mask_ccs_${FILENAMESTRING}.srv
+# rm irt_tracks_mask_${FILENAMESTRING}.srv
+
+
+
+
+
+# FILENAMESTRING='gpmmm_cn_'${year}'03'
+# cd $PFAD
+# ${PFAD}/irt_objects_release.x ${FILENAMESTRING}
+# ${PFAD}/irt_tracks_release.x ${FILENAMESTRING}
+# ${PFAD}/irt_tracklinks_release.x ${FILENAMESTRING}
+# cd /disk1/guoyj/MCS-tracking/iterative_raincell_tracking/GPM-225K-6hr-a-a/output/
+# sort -n -k2 irt_tracks_nohead_output_${FILENAMESTRING}.txt > irt_tracks_sorted_${FILENAMESTRING}.txt
+# ${PFAD}/irt_trackmask_release.x ${FILENAMESTRING}
+# #cdo -r -f nc copy irt_objects_mask_${FILENAMESTRING}.srv irt_objects_mask_${FILENAMESTRING}.nc
+# cdo -r -f nc copy irt_tracks_mask_ccs_${FILENAMESTRING}.srv irt_tracks_mask_ccs_${FILENAMESTRING}.nc
+# cdo -r -f nc copy irt_tracks_mask_${FILENAMESTRING}.srv irt_tracks_mask_${FILENAMESTRING}.nc
+# rm irt_objects_mask_${FILENAMESTRING}.srv
+# rm irt_tracks_mask_ccs_${FILENAMESTRING}.srv
+# rm irt_tracks_mask_${FILENAMESTRING}.srv
+
+
+# FILENAMESTRING='gpmmm_cn_'${year}'04'
+# cd $PFAD
+# ${PFAD}/irt_objects_release.x ${FILENAMESTRING}
+# ${PFAD}/irt_tracks_release.x ${FILENAMESTRING}
+# ${PFAD}/irt_tracklinks_release.x ${FILENAMESTRING}
+# cd /disk1/guoyj/MCS-tracking/iterative_raincell_tracking/GPM-225K-6hr-a-a/output/
+# sort -n -k2 irt_tracks_nohead_output_${FILENAMESTRING}.txt > irt_tracks_sorted_${FILENAMESTRING}.txt
+# ${PFAD}/irt_trackmask_release.x ${FILENAMESTRING}
+# #cdo -r -f nc copy irt_objects_mask_${FILENAMESTRING}.srv irt_objects_mask_${FILENAMESTRING}.nc
+# cdo -r -f nc copy irt_tracks_mask_ccs_${FILENAMESTRING}.srv irt_tracks_mask_ccs_${FILENAMESTRING}.nc
+# cdo -r -f nc copy irt_tracks_mask_${FILENAMESTRING}.srv irt_tracks_mask_${FILENAMESTRING}.nc
+# rm irt_objects_mask_${FILENAMESTRING}.srv
+# rm irt_tracks_mask_ccs_${FILENAMESTRING}.srv
+# rm irt_tracks_mask_${FILENAMESTRING}.srv
+
+# FILENAMESTRING='gpmmm_cn_'${year}'05'
+# cd $PFAD
+# ${PFAD}/irt_objects_release.x ${FILENAMESTRING}
+# ${PFAD}/irt_tracks_release.x ${FILENAMESTRING}
+# ${PFAD}/irt_tracklinks_release.x ${FILENAMESTRING}
+# cd /disk1/guoyj/MCS-tracking/iterative_raincell_tracking/GPM-225K-6hr-a-a/output/
+# sort -n -k2 irt_tracks_nohead_output_${FILENAMESTRING}.txt > irt_tracks_sorted_${FILENAMESTRING}.txt
+# ${PFAD}/irt_trackmask_release.x ${FILENAMESTRING}
+# #cdo -r -f nc copy irt_objects_mask_${FILENAMESTRING}.srv irt_objects_mask_${FILENAMESTRING}.nc
+# cdo -r -f nc copy irt_tracks_mask_ccs_${FILENAMESTRING}.srv irt_tracks_mask_ccs_${FILENAMESTRING}.nc
+# cdo -r -f nc copy irt_tracks_mask_${FILENAMESTRING}.srv irt_tracks_mask_${FILENAMESTRING}.nc
+# rm irt_objects_mask_${FILENAMESTRING}.srv
+# rm irt_tracks_mask_ccs_${FILENAMESTRING}.srv
+# rm irt_tracks_mask_${FILENAMESTRING}.srv
+
+# FILENAMESTRING='gpmmm_cn_'${year}'06'
+# cd $PFAD
+# ${PFAD}/irt_objects_release.x ${FILENAMESTRING}
+# ${PFAD}/irt_tracks_release.x ${FILENAMESTRING}
+# ${PFAD}/irt_tracklinks_release.x ${FILENAMESTRING}
+# cd /disk1/guoyj/MCS-tracking/iterative_raincell_tracking/GPM-225K-6hr-a-a/output/
+# sort -n -k2 irt_tracks_nohead_output_${FILENAMESTRING}.txt > irt_tracks_sorted_${FILENAMESTRING}.txt
+# ${PFAD}/irt_trackmask_release.x ${FILENAMESTRING}
+# #cdo -r -f nc copy irt_objects_mask_${FILENAMESTRING}.srv irt_objects_mask_${FILENAMESTRING}.nc
+# cdo -r -f nc copy irt_tracks_mask_ccs_${FILENAMESTRING}.srv irt_tracks_mask_ccs_${FILENAMESTRING}.nc
+# cdo -r -f nc copy irt_tracks_mask_${FILENAMESTRING}.srv irt_tracks_mask_${FILENAMESTRING}.nc
+# rm irt_objects_mask_${FILENAMESTRING}.srv
+# rm irt_tracks_mask_ccs_${FILENAMESTRING}.srv
+# rm irt_tracks_mask_${FILENAMESTRING}.srv
+
+
+# FILENAMESTRING='gpmmm_cn_'${year}'07'
+# cd $PFAD
+# ${PFAD}/irt_objects_release.x ${FILENAMESTRING}
+# ${PFAD}/irt_tracks_release.x ${FILENAMESTRING}
+# ${PFAD}/irt_tracklinks_release.x ${FILENAMESTRING}
+# cd /disk1/guoyj/MCS-tracking/iterative_raincell_tracking/GPM-225K-6hr-a-a/output/
+# sort -n -k2 irt_tracks_nohead_output_${FILENAMESTRING}.txt > irt_tracks_sorted_${FILENAMESTRING}.txt
+# ${PFAD}/irt_trackmask_release.x ${FILENAMESTRING}
+# #cdo -r -f nc copy irt_objects_mask_${FILENAMESTRING}.srv irt_objects_mask_${FILENAMESTRING}.nc
+# cdo -r -f nc copy irt_tracks_mask_ccs_${FILENAMESTRING}.srv irt_tracks_mask_ccs_${FILENAMESTRING}.nc
+# cdo -r -f nc copy irt_tracks_mask_${FILENAMESTRING}.srv irt_tracks_mask_${FILENAMESTRING}.nc
+# rm irt_objects_mask_${FILENAMESTRING}.srv
+# rm irt_tracks_mask_ccs_${FILENAMESTRING}.srv
+# rm irt_tracks_mask_${FILENAMESTRING}.srv
+
+# FILENAMESTRING='gpmmm_cn_'${year}'08'
+# cd $PFAD
+# ${PFAD}/irt_objects_release.x ${FILENAMESTRING}
+# ${PFAD}/irt_tracks_release.x ${FILENAMESTRING}
+# ${PFAD}/irt_tracklinks_release.x ${FILENAMESTRING}
+# cd /disk1/guoyj/MCS-tracking/iterative_raincell_tracking/GPM-225K-6hr-a-a/output/
+# sort -n -k2 irt_tracks_nohead_output_${FILENAMESTRING}.txt > irt_tracks_sorted_${FILENAMESTRING}.txt
+# ${PFAD}/irt_trackmask_release.x ${FILENAMESTRING}
+# #cdo -r -f nc copy irt_objects_mask_${FILENAMESTRING}.srv irt_objects_mask_${FILENAMESTRING}.nc
+# cdo -r -f nc copy irt_tracks_mask_ccs_${FILENAMESTRING}.srv irt_tracks_mask_ccs_${FILENAMESTRING}.nc
+# cdo -r -f nc copy irt_tracks_mask_${FILENAMESTRING}.srv irt_tracks_mask_${FILENAMESTRING}.nc
+# rm irt_objects_mask_${FILENAMESTRING}.srv
+# rm irt_tracks_mask_ccs_${FILENAMESTRING}.srv
+# rm irt_tracks_mask_${FILENAMESTRING}.srv
+
+
+# FILENAMESTRING='gpmmm_cn_'${year}'09'
+# cd $PFAD
+# ${PFAD}/irt_objects_release.x ${FILENAMESTRING}
+# ${PFAD}/irt_tracks_release.x ${FILENAMESTRING}
+# ${PFAD}/irt_tracklinks_release.x ${FILENAMESTRING}
+# cd /disk1/guoyj/MCS-tracking/iterative_raincell_tracking/GPM-225K-6hr-a-a/output/
+# sort -n -k2 irt_tracks_nohead_output_${FILENAMESTRING}.txt > irt_tracks_sorted_${FILENAMESTRING}.txt
+# ${PFAD}/irt_trackmask_release.x ${FILENAMESTRING}
+# #cdo -r -f nc copy irt_objects_mask_${FILENAMESTRING}.srv irt_objects_mask_${FILENAMESTRING}.nc
+# cdo -r -f nc copy irt_tracks_mask_ccs_${FILENAMESTRING}.srv irt_tracks_mask_ccs_${FILENAMESTRING}.nc
+# cdo -r -f nc copy irt_tracks_mask_${FILENAMESTRING}.srv irt_tracks_mask_${FILENAMESTRING}.nc
+# rm irt_objects_mask_${FILENAMESTRING}.srv
+# rm irt_tracks_mask_ccs_${FILENAMESTRING}.srv
+# rm irt_tracks_mask_${FILENAMESTRING}.srv
+
+
+done 
+
+exit
